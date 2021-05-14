a4926f329e8fa9e13721fba4aa068cce2bef3d38
noubaio <36126062+noubaio@users.noreply.github.com>
2021-05-14 15:08:46 +0200

    update version

--- PYTHON/Vesrion.py
+++ PYTHON/Vesrion.py
@@ -1,17 +1,10 @@
 import random
 import string
 class Version:
-    __prop['systemProp.http.proxyHost']='proxy'
-    __prop['systemProp.http.proxyPort']='8081'
-    __prop['systemProp.http.nonProxyHosts']='192.168.1.12'
-    __prop['systemProp.https.proxyHost']='proxy'
-    __prop['systemProp.https.proxyPort']='8081'
-    __prop['systemProp.https.nonProxyHosts']='192.168.1.12'
-    __prop['nom'] = "Gestion Parc Informatique"
-    __prop['Version'] = 1.0
-    __prop['auteur'] = "Omar & Amine"
-    __prop['licens'] = ""
+
+    __prop = {}
     __file = "/var/lib/jenkins/workspace/GestionParcInformatique/"
+    #__file = "/home/rsync/tp_python/"
 
 
 #################################################
@@ -27,29 +20,40 @@ class Version:
 ######       Récupére dernière info      ########
 #################################################
     def __init__(self):
-        monFichier = open(self.___file + "gradle.properties", "r")
-        maLine=monFichier.readline()
-        self.__prop['systemProp.http.proxyHost']=maLine.split("=")[1].strip()
-        maLine=monFichier.readline()
-        self.__prop['systemProp.http.proxyPort']=maLine.split("=")[1].strip()
-        maLine=monFichier.readline()
-        self.__prop['systemProp.http.nonProxyHosts']=maLine.split("=")[1].strip()
-        maLine=monFichier.readline()
-        self.__prop['systemProp.https.proxyHost']=maLine.split("=")[1].strip()
-        maLine=monFichier.readline()
-        self.__prop['systemProp.https.proxyPort']=maLine.split("=")[1].strip()
-        maLine=monFichier.readline()
-        self.__prop['systemProp.https.nonProxyHosts']=maLine.split("=")[1].strip()
-        maLine=monFichier.readline()
-        self.__prop['nom'] = maLine.split("=")[1].strip()
-        maLine=monFichier.readline()
-        self.__prop['Version'] = float(maLine.split("=")[1]).strip()
-        maLine=monFichier.readline()
-        self.__prop['auteur'] = maLine.split("=")[1].strip()
-        maLine=monFichier.readline()
-        self.__prop['licens'] = maLine.split("=")[1].strip()
+        monFichier = open(self.__file + "gradle.properties", "r")
+        try :
+            maLine=monFichier.readline()
+            self.__prop['systemProp.http.proxyHost']=maLine.split("=")[1].strip()
+            maLine=monFichier.readline()
+            self.__prop['systemProp.http.proxyPort']=maLine.split("=")[1].strip()
+            maLine=monFichier.readline()
+            self.__prop['systemProp.http.nonProxyHosts']=maLine.split("=")[1].strip()
+            maLine=monFichier.readline()
+            self.__prop['systemProp.https.proxyHost']=maLine.split("=")[1].strip()
+            maLine=monFichier.readline()
+            self.__prop['systemProp.https.proxyPort']=maLine.split("=")[1].strip()
+            maLine=monFichier.readline()
+            self.__prop['systemProp.https.nonProxyHosts']=maLine.split("=")[1].strip()
+            maLine=monFichier.readline()
+            self.__prop['nom'] = maLine.split("=")[1].strip()
+            maLine=monFichier.readline()
+            self.__prop['Version'] = float(maLine.split("=")[1].strip())
+            maLine=monFichier.readline()
+            self.__prop['auteur'] = maLine.split("=")[1].strip()
+            maLine=monFichier.readline()
+            self.__prop['licens'] = maLine.split("=")[1].strip()
+        except IndexError:
+            self.__prop['systemProp.http.proxyHost']='proxy'
+            self.__prop['systemProp.http.proxyPort']='8081'
+            self.__prop['systemProp.http.nonProxyHosts']='192.168.1.12'
+            self.__prop['systemProp.https.proxyHost']='proxy'
+            self.__prop['systemProp.https.proxyPort']='8081'
+            self.__prop['systemProp.https.nonProxyHosts']='192.168.1.12'
+            self.__prop['nom'] = "Gestion Parc Informatique"
+            self.__prop['Version'] = 1.0
+            self.__prop['auteur'] = "Omar & Amine"
+            self.__prop['licens'] = self.newLicense()
         monFichier.close()
-
     
 #################################################
 ######     Auto genere license 50 char    #######
@@ -57,23 +61,18 @@ class Version:
     def newLicense(self):
         letters = string.ascii_letters
         self.__prop['licens'] = ''.join(random.choice(letters) for i in range(50))
-        
-        
+           
 #################################################
 ###### Update and save des modifications ########
 #################################################
-
     def save(self):
-        __prop['Version'] +=  0.1
+        self.__prop['Version'] +=  0.1
         self.newLicense()
-        monFichier = open(self.___file + "gradle.properties", "w")
-        v = "%.1f" % float(self.Version)
-        monFichier.write("systemProp.http.proxyHost={}\n,systemProp.http.proxyPort={}\n,systemProp.http.nonProxyHosts={}\n,systemProp.https.proxyHost={}\n,systemProp.https.proxyPort={}\n,systemProp.https.nonProxyHosts={}\n,nom={}\n,Version={}\n,auteur={}\n,licens={}\n".format(self.__prop['systemProp.http.proxyHost'],self.__prop['systemProp.http.proxyPort'],self.__prop['systemProp.http.nonProxyHosts'],self.__prop['systemProp.https.proxyHost'],self.__prop['systemProp.https.proxyPort'],self.__prop['systemProp.https.nonProxyHosts'],self.__prop['nom'],self.__prop['Version'],self.__prop['auteur'],self.__prop['licens']))
+        monFichier = open(self.__file + "gradle.properties", "w")
+        v = "%.1f" % float(self.__prop['Version'])
+        monFichier.write("systemProp.http.proxyHost={}\nsystemProp.http.proxyPort={}\nsystemProp.http.nonProxyHosts={}\nsystemProp.https.proxyHost={}\nsystemProp.https.proxyPort={}\nsystemProp.https.nonProxyHosts={}\nnom={}\nVersion={}\nauteur={}\nlicens={}\n".format(self.__prop['systemProp.http.proxyHost'],self.__prop['systemProp.http.proxyPort'],self.__prop['systemProp.http.nonProxyHosts'],self.__prop['systemProp.https.proxyHost'],self.__prop['systemProp.https.proxyPort'],self.__prop['systemProp.https.nonProxyHosts'],self.__prop['nom'],v,self.__prop['auteur'],self.__prop['licens']))
         monFichier.close()
-        return "la nouvelle license est : " + self.licens
-
-
-
+        return "la nouvelle license est : " + self.__prop['licens']
 
 #################################################
 ######     Exec le save et automatise     #######
