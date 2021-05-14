450e900199a129d1a4c5c14e4dad5e10e005f147
noubaio <36126062+noubaio@users.noreply.github.com>
2021-05-14 12:29:03 +0200

    update gestion version

--- PYTHON/Vesrion.py
+++ PYTHON/Vesrion.py
@@ -1,11 +1,17 @@
 import random
 import string
 class Version:
-    nom = "Gestion Parc Informatique"
-    Version = 1.0
-    auteur = "Omar & Amine"
-    licens = ""
-
+    __prop['systemProp.http.proxyHost']='proxy'
+    __prop['systemProp.http.proxyPort']='8081'
+    __prop['systemProp.http.nonProxyHosts']='192.168.1.12'
+    __prop['systemProp.https.proxyHost']='proxy'
+    __prop['systemProp.https.proxyPort']='8081'
+    __prop['systemProp.https.nonProxyHosts']='192.168.1.12'
+    __prop['nom'] = "Gestion Parc Informatique"
+    __prop['Version'] = 1.0
+    __prop['auteur'] = "Omar & Amine"
+    __prop['licens'] = ""
+    __file = "/var/lib/jenkins/workspace/GestionParcInformatique/"
 
 
 #################################################
@@ -21,13 +27,27 @@ class Version:
 ######       Récupére dernière info      ########
 #################################################
     def __init__(self):
-        monFichier = open("version.txt", "r")
+        monFichier = open(self.___file + "gradle.properties", "r")
         maLine=monFichier.readline()
-        splitline = maLine.split(",")
-        self.nom = splitline[0]
-        self.Version = float(splitline[1])
-        self.auteur = splitline[2]
-        self.licens = splitline[3]
+        self.__prop['systemProp.http.proxyHost']=maLine.split("=")[1].strip()
+        maLine=monFichier.readline()
+        self.__prop['systemProp.http.proxyPort']=maLine.split("=")[1].strip()
+        maLine=monFichier.readline()
+        self.__prop['systemProp.http.nonProxyHosts']=maLine.split("=")[1].strip()
+        maLine=monFichier.readline()
+        self.__prop['systemProp.https.proxyHost']=maLine.split("=")[1].strip()
+        maLine=monFichier.readline()
+        self.__prop['systemProp.https.proxyPort']=maLine.split("=")[1].strip()
+        maLine=monFichier.readline()
+        self.__prop['systemProp.https.nonProxyHosts']=maLine.split("=")[1].strip()
+        maLine=monFichier.readline()
+        self.__prop['nom'] = maLine.split("=")[1].strip()
+        maLine=monFichier.readline()
+        self.__prop['Version'] = float(maLine.split("=")[1]).strip()
+        maLine=monFichier.readline()
+        self.__prop['auteur'] = maLine.split("=")[1].strip()
+        maLine=monFichier.readline()
+        self.__prop['licens'] = maLine.split("=")[1].strip()
         monFichier.close()
 
     
@@ -36,7 +56,7 @@ class Version:
 #################################################
     def newLicense(self):
         letters = string.ascii_letters
-        self.licens = ''.join(random.choice(letters) for i in range(50))
+        self.__prop['licens'] = ''.join(random.choice(letters) for i in range(50))
         
         
 #################################################
@@ -44,11 +64,11 @@ class Version:
 #################################################
 
     def save(self):
-        self.Version +=  0.1
+        __prop['Version'] +=  0.1
         self.newLicense()
-        monFichier = open("version.txt", "w")
+        monFichier = open(self.___file + "gradle.properties", "w")
         v = "%.1f" % float(self.Version)
-        monFichier.write("{},{},{},{}".format(self.nom,v,self.auteur,self.licens))
+        monFichier.write("systemProp.http.proxyHost={}\n,systemProp.http.proxyPort={}\n,systemProp.http.nonProxyHosts={}\n,systemProp.https.proxyHost={}\n,systemProp.https.proxyPort={}\n,systemProp.https.nonProxyHosts={}\n,nom={}\n,Version={}\n,auteur={}\n,licens={}\n".format(self.__prop['systemProp.http.proxyHost'],self.__prop['systemProp.http.proxyPort'],self.__prop['systemProp.http.nonProxyHosts'],self.__prop['systemProp.https.proxyHost'],self.__prop['systemProp.https.proxyPort'],self.__prop['systemProp.https.nonProxyHosts'],self.__prop['nom'],self.__prop['Version'],self.__prop['auteur'],self.__prop['licens']))
         monFichier.close()
         return "la nouvelle license est : " + self.licens
 
